cd65f58b7aae391bdede35dcc991bf99bc934cad
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-04-14 13:11:58 -0500

    primer commit, terminado

---
+++ Inventario/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Inventario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMostrar = new System.Windows.Forms.RichTextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnElimar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numPosicion = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblEstado = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numPosicion)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Location = new System.Drawing.Point(18, 18);
+            this.txtMostrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(462, 442);
+            this.txtMostrar.TabIndex = 0;
+            this.txtMostrar.Text = "";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(636, 18);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(232, 26);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(636, 248);
+            this.txtCosto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(232, 26);
+            this.txtCosto.TabIndex = 2;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(636, 186);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(232, 26);
+            this.txtCantidad.TabIndex = 3;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(636, 125);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(232, 26);
+            this.txtDescripcion.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(636, 70);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(232, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.AutoSize = true;
+            this.btnAgregar.Location = new System.Drawing.Point(794, 297);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(76, 30);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.AutoSize = true;
+            this.btnListar.Location = new System.Drawing.Point(636, 297);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 30);
+            this.btnListar.TabIndex = 7;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.AutoSize = true;
+            this.btnInsertar.Location = new System.Drawing.Point(831, 380);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 30);
+            this.btnInsertar.TabIndex = 8;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnElimar
+            // 
+            this.btnElimar.AutoSize = true;
+            this.btnElimar.Location = new System.Drawing.Point(750, 380);
+            this.btnElimar.Name = "btnElimar";
+            this.btnElimar.Size = new System.Drawing.Size(75, 30);
+            this.btnElimar.TabIndex = 9;
+            this.btnElimar.Text = "Eliminar";
+            this.btnElimar.UseVisualStyleBackColor = true;
+            this.btnElimar.Click += new System.EventHandler(this.btnElimar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.AutoSize = true;
+            this.btnBuscar.Location = new System.Drawing.Point(893, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 30);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(566, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(574, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Costo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Cantidad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(533, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Descripción:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(560, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Nombre:";
+            // 
+            // numPosicion
+            // 
+            this.numPosicion.Location = new System.Drawing.Point(794, 416);
+            this.numPosicion.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numPosicion.Name = "numPosicion";
+            this.numPosicion.Size = new System.Drawing.Size(75, 26);
+            this.numPosicion.TabIndex = 16;
+            this.numPosicion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(716, 418);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Posición:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstado});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 469);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(980, 22);
+            this.statusStrip1.TabIndex = 19;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 491);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numPosicion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnElimar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtMostrar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numPosicion)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMostrar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnElimar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numPosicion;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstado;
+    }
+}
+
---
+++ Inventario/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Inventario
+{
+    public partial class Form1 : Form
+    {
+        public class Producto
+        {
+            private static int ids = -1;
+
+            private int codigo;
+            private string nombre;
+            private double costo;
+
+            public int Codigo { get { return codigo; } }
+            public string Nombre { get { return nombre; } }
+            public double Costo { get { return costo; } }
+  
+            public string Descripcion { get; set; }
+            public int Cantidad { get; set; }
+
+            public Producto(string name, double cost, string description, int amount)
+            {
+                ids++;
+                this.codigo = ids;
+                this.nombre = name;
+                this.costo = cost;
+                this.Descripcion = description;
+                this.Cantidad = amount;
+            }
+
+            public override string ToString()
+            {
+                return "******* " + nombre + " *******\n" +
+                        "Código: " + codigo + "\n" +
+                        "Cantidad: " + Cantidad + "\n" +
+                        "Costo: " + costo + "\n\n" +
+                        "Descripción: " + Descripcion;
+            }
+        }
+
+        public class Inventario
+        {
+            public Producto[] productos;
+            public int Length { get { return productos.Length; } }
+            private int lastElement;
+
+            public Inventario(int numeroProductos)
+            {
+                productos = new Producto[numeroProductos];
+                for (int i = 0; i < Length; i++)
+                {
+                    productos[i] = null;
+                }
+                lastElement = 0;
+            }
+
+            public string agregar(string name, double cost, string description, int amount)
+            {
+                if (lastElement < Length)
+                {
+                    productos[lastElement] = new Producto(name, cost, description, amount);
+                    lastElement++;
+                    return "Se agregó exitosamente el producto";
+                }
+                else
+                {
+                    return "Ya no se pueden agregar más productos";
+                }
+            }
+
+            public Producto buscar(int codigo)
+            {
+                for(int i = 0; i < lastElement; i++)
+                {
+                    if (productos[i].Codigo == codigo)
+                        return productos[i];
+                }
+
+                return null;
+            }
+
+            public string eliminar(int pos)
+            {
+                if (pos < 0)
+                {
+                    return "La posición debe ser mayor a 0";
+                }
+                else if (pos > lastElement)
+                {
+                    return "La posición debe ser menor a la cantidad actual de productos (" + lastElement + ")";
+                }
+                else
+                {
+                    for (int i = pos; i < lastElement - 1; i++)
+                        productos[i] = new Producto(productos[i + 1].Nombre, productos[i + 1].Costo, productos[i + 1].Descripcion, productos[i + 1].Cantidad);
+
+                    productos[lastElement - 1] = null;
+
+                    lastElement--;
+                    return "Se eliminó correctamente el producto";
+                }
+            }
+
+            public string insertar(int posicion, Producto producto)
+            {
+                if (posicion < 0)
+                {
+                    return "La posición debe ser mayor a 0";
+                }
+                else if (posicion > lastElement)
+                {
+                    return "La posición debe ser menor a la cantidad actual de productos (" + lastElement + ")";
+                }
+                else
+                {
+                    Producto temporal = new Producto(productos[lastElement - 1].Nombre, productos[lastElement - 1].Costo, productos[lastElement - 1].Descripcion, productos[lastElement - 1].Cantidad);
+
+                    for (int i = posicion; i < lastElement; i++)
+                    {
+                        Producto temp = new Producto(productos[i].Nombre, productos[i].Costo, productos[i].Descripcion, productos[i].Cantidad);
+                        productos[i] = new Producto(producto.Nombre, producto.Costo, producto.Descripcion, producto.Cantidad);
+                        producto = temp;
+                    }
+
+                    if (lastElement < Length)
+                        productos[lastElement++] = temporal;
+
+                    return "Se insertó correctamente el producto";
+                }
+            }
+
+            public string listar()
+            {
+                string cadena = "";
+                for (int i = 0; i < lastElement; i++)
+                {
+                    cadena += productos[i].ToString() + "\n__________________________________\n";
+                }
+                return cadena;
+            }
+        }
+
+        Inventario inventario;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inventario = new Inventario(15);
+            addProductos();
+        }
+
+        public void addProductos()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                inventario.agregar("producto " + i, i, "description " + i + " " + i + " " + " "+ i, i);
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            txtMostrar.Text = inventario.listar();
+            lblEstado.Text = "Elementos listados";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if(validar())
+            {
+                string agregar = inventario.agregar(txtNombre.Text, Convert.ToDouble(txtCantidad.Text), txtDescripcion.Text, Convert.ToInt32(txtCantidad.Text));
+                lblEstado.Text = agregar;
+            }
+        }
+
+        private void btnElimar_Click(object sender, EventArgs e)
+        {
+            int posicion = Convert.ToInt32(numPosicion.Value);
+            string eliminar = inventario.eliminar(posicion); 
+            lblEstado.Text = eliminar;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if(txtCodigo.Text != "")
+            {
+                int codigo = Convert.ToInt32(txtCodigo.Text);
+                Producto buscar = inventario.buscar(codigo);
+                if(buscar != null)
+                {
+                    lblEstado.Text = "Producto encontrado";
+                    MessageBox.Show(buscar.ToString());
+                }
+                else
+                {
+                    lblEstado.Text = "Producto no encontrado";
+                }
+            }
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if(validar())
+            {
+                int posicion = Convert.ToInt32(numPosicion.Value);
+                string insertar = inventario.insertar(posicion, new Producto(txtNombre.Text, Convert.ToDouble(txtCantidad.Text), txtDescripcion.Text, Convert.ToInt32(txtCantidad.Text)));
+                lblEstado.Text = insertar;
+            }
+        }
+
+        public bool validar()
+        {
+            return txtNombre.Text != "" && txtCodigo.Text != "" && txtCantidad.Text != "" && txtCosto.Text != "" && txtDescripcion.Text != "";
+        }
+    }
+}
